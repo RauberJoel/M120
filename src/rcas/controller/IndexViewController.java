a3e9831a0f530175c16c797faa2cefd58cac55b7
zmeieb <ben.meier@bbcag.ch>
2019-06-28 10:40:43 +0200

    CHANGED: AnchorPane is now a GridPane

--- src/rcas/controller/IndexViewController.java
+++ src/rcas/controller/IndexViewController.java
@@ -3,6 +3,7 @@ package rcas.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class IndexViewController {
@@ -12,6 +13,8 @@ public class IndexViewController {
     private ChoiceBox selectBox;
     @FXML
     private Button loginBtn;
+    @FXML
+    private GridPane mainPane;
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;

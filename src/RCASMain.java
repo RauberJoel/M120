8a808143ace3d7693bd476119bc323748f5615ab
zmeieb <ben.meier@bbcag.ch>
2019-06-28 10:22:14 +0200

    CHANGED: start config of RCASMain

--- src/RCASMain.java
+++ src/RCASMain.java
@@ -1,38 +1,35 @@
-import java.util.ResourceBundle;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import rcas.controller.PropertiesViewController;
+import rcas.controller.IndexViewController;
 import rcas.util.Variables;
+
+import java.util.ResourceBundle;
 
 public class RCASMain extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader fxmlLoader = new FXMLLoader();
-		fxmlLoader.setLocation(RCASMain.class.getResource("view/Properties.fxml"));
-		ResourceBundle resourceBundle = ResourceBundle.getBundle("RCASResources");
-		fxmlLoader.setResources(resourceBundle);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(RCASMain.class.getResource("view/index.fxml"));
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("RCASResources");
+        fxmlLoader.setResources(resourceBundle);
 
 
-
-		GridPane mainPane = fxmlLoader.load();
-		Scene mainScene = new Scene(mainPane, 1500, 800);
-		primaryStage.centerOnScreen();
-		primaryStage.setTitle(resourceBundle.getString("applicationTitle"));
-		primaryStage.setScene(mainScene);
-		primaryStage.show();
-		PropertiesViewController controller =fxmlLoader.getController();
-		controller.setPrimaryStage(primaryStage);
+        Scene mainScene = new Scene(fxmlLoader.load(), 1500, 800);
+        primaryStage.centerOnScreen();
+        primaryStage.setTitle(resourceBundle.getString("applicationTitle"));
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+        IndexViewController controller = fxmlLoader.getController();
+        controller.setPrimaryStage(primaryStage);
 
 
-		Variables.scene = mainScene;
-	}
+        Variables.scene = mainScene;
+    }
 
-	public static void main(String[] args) {
-		Application.launch(RCASMain.class, args);
-	}
+    public static void main(String[] args) {
+        Application.launch(RCASMain.class, args);
+    }
 }
